abf4d00f247b2fed4399c8040b05bd804fc06d09
Dan Zeman <zeman@ufal.mff.cuni.cz>
2020-10-03 17:45:36 +0200

    Added sentence-terminating period to grammatical examples.

--- not-to-release/update.sh
+++ not-to-release/update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 CAIRO=../../cairo
-perl $CAIRO/extract_language.pl tl < $CAIRO/shopen-examples.conllu | perl -pe 's/^\# sent_id = /\# sent_id = shopen-/; if(m/^\# Tagalog/) { $_ = "" }' > shopen-tl.conllu
+perl $CAIRO/extract_language.pl tl < $CAIRO/shopen-examples.conllu | perl -pe 's/^\# sent_id = /\# sent_id = shopen-/; if(m/^\# Tagalog/) { $_ = "" }' | udapy -s my.AddPeriod > shopen-tl.conllu
 
